8fb204d7daabd725ceb1042f18f1d75889d2dd44
Asbiq Al Alawi <asbiqalalawi@gmail.com>
2023-11-02 13:44:16 +0700

    feat: Add Map to SwiftUI project with MapKit

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,40 @@
 //
 
 import Foundation
+import _MapKit_SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
+    // All Locations
     @Published var locations: [Location]
+    
+    // Current Location on map
+    @Published var location: Location {
+        didSet {
+            updateMapRegion(location: location)
+        }
+    }
+    
+    @Published var mapRegion = MapCameraPosition.region(MKCoordinateRegion())
+    let mapSpan = MKCoordinateSpan(
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1
+    )
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.location = locations.first!
+        
+        self.updateMapRegion(location: location)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        mapRegion = MapCameraPosition.region(
+            MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan
+            )
+        )
     }
 }
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -6,16 +6,15 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
     
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
-            }
+        ZStack {
+            Map(position: $vm.mapRegion)
         }
     }
 }
